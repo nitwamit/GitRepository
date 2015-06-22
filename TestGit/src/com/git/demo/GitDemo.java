53539f5c408972b1f563da45ad5986d461a3be28
Amit Kumar <amit.kumar@ecitele.com>
2015-06-22 23:21:41 +0530

    my commit

--- TestGit/src/com/git/demo/GitDemo.java
+++ TestGit/src/com/git/demo/GitDemo.java
@@ -5,7 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("Git is fun");
 		System.out.println("Git is rope");
-		System.out.println("Git is coo");
+		System.out.println("Git is cooool");
 	}
 
 }
