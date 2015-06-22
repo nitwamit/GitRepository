5b6be0354552591e8dbba6f7b94276b4160f6b22
Amit Kumar <amit.kumar@ecitele.com>
2015-06-22 23:08:07 +0530

    3rd commit

--- TestGit/src/com/git/demo/GitDemo.java
+++ TestGit/src/com/git/demo/GitDemo.java
@@ -4,7 +4,7 @@ public class GitDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Git is fun");
-		System.out.println("Git is hot");
+		System.out.println("Git is rope");
 		System.out.println("Git is cool");
 	}
 
