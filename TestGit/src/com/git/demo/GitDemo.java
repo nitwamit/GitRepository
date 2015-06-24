b27c28680a095bc3345191accfb1ec2fccb35125
Amit Kumar <amit.kumar@ecitele.com>
2015-06-24 09:51:40 +0530

    My commit from Eclipse

--- TestGit/src/com/git/demo/GitDemo.java
+++ TestGit/src/com/git/demo/GitDemo.java
@@ -5,7 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("Git is fun");
 		System.out.println("Git is rope");
-		System.out.println("Git is new");
+		System.out.println("Git is hot");
 	}
 
 }
