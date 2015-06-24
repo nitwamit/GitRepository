88dc7213b2ac0eaf8a7502d5e518e904db28b903
Amit Kumar <amit.kumar@ecitele.com>
2015-06-24 09:42:24 +0530

    Commit by User2

---
+++ TestGit2/src/com/git/demo/GitDemo.java
@@ -0,0 +1,9 @@
+package com.git.demo;
+
+public class GitDemo {
+	public static void main(String[] args) {
+		System.out.println("GitDemo User2");
+		System.out.println("Git is rope");
+		System.out.println("Git is up12");
+	}
+}
---
+++ TestGit2/src/com/git/demo/GitDemo2.java
@@ -0,0 +1,11 @@
+package com.git.demo;
+
+public class GitDemo2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is User2");
+		System.out.println("Git is rope");
+		System.out.println("Git is cool");
+	}
+
+}
